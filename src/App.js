b6f320332403bfa4a341990c397e16a40800043f
TheFaLez <thefalez0@gmail.com>
2024-01-18 17:13:50 +0500

    Weather

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Weather from './components/Weather/Weather'
 import "./style/style.css"
+import AOS from 'aos'
+import "aos/dist/aos.css"
 
 const App = () => {
 
- 
+  AOS.init()
 
   return (
     <div className='App'>
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -39,15 +39,15 @@ const Weather = () => {
       <div className="weather-back">
         <div className='weather-day'>
           <div className="weather-date">
-            <h3>{weather.location.name}</h3>
-            <h4>{weather.forecast.forecastday[0].date}</h4>
+            <h3 data-aos="fade-right">{weather.location.name}</h3>
+            <h4 data-aos="fade-right">{weather.forecast.forecastday[0].date}</h4>
             <div className="search-content">
-              <input onChange={seach} value={searchState} className='weather-date-input' type="text" placeholder='Search...' />
+              <input data-aos="fade-right" onChange={seach} value={searchState} className='weather-date-input' type="text" placeholder='Search...' />
               <i onClick={searchBtn} className="fa-solid fa-magnifying-glass searchicon"></i>
             </div>
           </div>
 
-          <div className="weather-main">
+          <div className="weather-main" data-aos="fade-up" >
             <h2 className='weather-main-title'>{weather.current.temp_c}°</h2>
             <div className='weather-main-text'>
               <h2>{weather.current.condition.text}</h2>
@@ -55,7 +55,7 @@ const Weather = () => {
             </div>
             <div />
 
-            <div className="weather-postcript-container">
+            <div className="weather-postcript-container" data-aos="fade-left">
               <h2 className="weather-postscript">
                 <i className="fa-solid fa-wind"></i>
                 {weather.current.wind_mph} mph
@@ -72,12 +72,12 @@ const Weather = () => {
         <div className="weather-hour">
           <h2 className='weather-hour-title'>Good Time Of Day</h2>
 
-          <div className="weather-hour-content">
+          <div className="weather-hour-content" data-aos="fade-up">
             <h2 className='weather-content-title'>
               {weather.current.temp_c}°
             </h2>
 
-            <div className="weather-content-container">
+            <div className="weather-content-container"  >
               <h4 className="weather-content-postscript">
                 <i className="fa-solid fa-wind"></i>
                 {weather.current.wind_mph} mph
@@ -89,8 +89,8 @@ const Weather = () => {
             </div>
           </div>
 
-          <h5 className='felllike'>Feels like {weather.current.feelslike_c}°</h5>
-          <h3 className='content-postscript'>{weather.current.condition.text}</h3>
+          <h5 data-aos="fade-up" className='felllike'>Feels like {weather.current.feelslike_c}°</h5>
+          <h3 data-aos="fade-up" className='content-postscript'>{weather.current.condition.text}</h3>
 
 
           <div className="weather-hour-cards">
@@ -99,7 +99,7 @@ const Weather = () => {
               weather.forecast.forecastday[0].hour.map((value, index) => {
                 if (index == 6) {
                   return (
-                    <div className="weather-card" key={value.time}>
+                    <div data-aos="fade-right" className="weather-card" key={value.time}>
                       <h4 className='hour-time'> 6 AM</h4>
                       <h3>{value.temp_c}°</h3>
                       <h4 className='hour-weather'>{value.condition.text}</h4>
@@ -108,7 +108,7 @@ const Weather = () => {
                 }
                 if (index == 9) {
                   return (
-                    <div className="weather-card" key={value.time}>
+                    <div data-aos="fade-right" className="weather-card" key={value.time}>
                       <h4 className='hour-time'> 9 AM</h4>
                       <h3>{value.temp_c}°</h3>
                       <h4 className='hour-weather'>{value.condition.text}</h4>
@@ -117,7 +117,7 @@ const Weather = () => {
                 }
                 if (index == 12) {
                   return (
-                    <div className="weather-card" key={value.time}>
+                    <div data-aos="fade-right" className="weather-card" key={value.time}>
                       <h4 className='hour-time'> 12 AM</h4>
                       <h3>{value.temp_c}°</h3>
                       <h4 className='hour-weather'>{value.condition.text}</h4>
@@ -126,7 +126,7 @@ const Weather = () => {
                 }
                 if (index == 15) {
                   return (
-                    <div className="weather-card" key={value.time}>
+                    <div data-aos="fade-right" className="weather-card" key={value.time}>
                       <h4 className='hour-time'> 3 PM</h4>
                       <h3>{value.temp_c}°</h3>
                       <h4 className='hour-weather'>{value.condition.text}</h4>
@@ -135,7 +135,7 @@ const Weather = () => {
                 }
                 if (index == 18) {
                   return (
-                    <div className="weather-card" key={value.time}>
+                    <div data-aos="fade-right" className="weather-card" key={value.time}>
                       <h4 className='hour-time'> 6 PM</h4>
                       <h3>{value.temp_c}°</h3>
                       <h4 className='hour-weather'>{value.condition.text}</h4>
@@ -144,7 +144,7 @@ const Weather = () => {
                 }
                 if (index == 21) {
                   return (
-                    <div className="weather-card" key={value.time}>
+                    <div data-aos="fade-right" className="weather-card" key={value.time}>
                       <h4 className='hour-time'> 9 PM</h4>
                       <h3>{value.temp_c}°</h3>
                       <h4 className='hour-weather'>{value.condition.text}</h4>
